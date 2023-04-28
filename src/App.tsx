431b5526f9352aaf6b6f36b4b0f8c9b5af4f98c7
Quynh Chi Lai <quynhchi1009@yandex.com>
2023-04-28 15:26:40 +0200

    React query - Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./App.css";
 import Posts from "./Posts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-    const client = new QueryClient()
+    const client = new QueryClient();
 
     return (
         <div className='App'>
             <QueryClientProvider client={client}>
                 <Posts />
+                <ReactQueryDevtools initialIsOpen={false} position="bottom-left"/>
             </QueryClientProvider>
         </div>
     );
